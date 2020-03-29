f79cbfe47dfffabb5c967946e7d43fd354df1507
William Wan <wwan@athenahealth.com>
2020-03-29 00:16:16 -0400

    moved stuff out of app.js

---
+++ src/Constants/Constants.js
@@ -0,0 +1,5 @@
+export const dataAttributes = {
+  death: { title: "Deaths", order: 3 },
+  hospitalized: { title: "Hospitalizations", order: 2 },
+  positive: { title: "Confirmed Cases", order: 1 }
+};
