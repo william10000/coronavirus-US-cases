1caf902b072ca8ba75a0528f3a647cf8ff2bf6df
William Wan <wwan@athenahealth.com>
2020-03-29 01:27:05 -0400

    moved more out of App.js

--- src/App.js
+++ src/App.js
@@ -1,108 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { dataAttributes } from "./Constants/Constants";
-import { TimeSeries } from "./Components/TimeSeries";
+import React, { useState } from "react";
+import { ChartsToPlot } from "./constants/Constants";
+import { TimeSeries } from "./components/TimeSeries";
+import { processData } from "./utils/utils";
 
-// https://covidtracking.com/api/
 const covidtrackingURL = "https://covidtracking.com/api/states/daily";
 
 const App = () => {
-  const [processedData, setProcessedData] = useState({
-    hospitalized: [{ state: "", data: [] }],
-    positive: [{ state: "", data: [] }],
-    death: [{ state: "", data: [] }]
-  });
+  const [processedData, setProcessedData] = useState(
+    ChartsToPlot.reduce(
+      (initialProcessedData, chart) => ({
+        ...initialProcessedData,
+        [chart.fieldName]: []
+      }),
+      {}
+    )
+  );
+  const [rawData, setRawData] = useState([]);
   const [firstLoad, setFirstLoad] = useState(true);
-
-  // current filter is for US states and excludes specific US cities
-  const dataRowFilter = dataRow =>
-    [
-      "GA",
-      "TX",
-      "MA",
-      "CA",
-      "WA",
-      "CO",
-      "NY",
-      "PA"
-      // "MI",
-      // "DE",
-      // "SC",
-      // "IN",
-      // "MO"
-    ].includes(dataRow["state"]);
-
-  // converts m/d/yy to yyyy-mm-dd
-  const convertDateToUNIXTime = date => {
-    return new Date(
-      `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`
-    ).getTime();
-  };
-
-  // keys: death, hospitalized, positive
-  const processData = rawData => {
-    let processedDataTemp = {};
-    const filteredData = rawData.filter(dataRow => dataRowFilter(dataRow));
-
-    filteredData.forEach(rawDataRow => {
-      const formattedDate = convertDateToUNIXTime(
-        rawDataRow["date"].toString(10)
-      );
-
-      Object.keys(dataAttributes).forEach(key => {
-        if (!processedDataTemp[key]) {
-          processedDataTemp[key] = {};
-        }
-
-        if (!processedDataTemp[key][rawDataRow["state"]]) {
-          processedDataTemp[key][rawDataRow["state"]] = [];
-        }
-
-        processedDataTemp[key][rawDataRow["state"]].push([
-          formattedDate,
-          rawDataRow[key]
-        ]);
-      });
-    });
-
-    return processedDataTemp;
-  };
 
   if (firstLoad) {
     fetch(covidtrackingURL)
       .then(response => response.json())
       .then(data => {
         setFirstLoad(false);
+        setRawData(data);
         setProcessedData(processData(data));
       });
   }
 
-  // useEffect(() => {
-  //   d3.csv(dataURL).then(data => {
-  //     setFirstLoad(false);
-  //     console.log(data);
-  //     console.log(processConfirmedCasesData(data));
-  //     setConfirmedCaseData(processConfirmedCasesData(data));
-  //   });
-  // }, []);
-
-  const sortedAttributes = Object.keys(dataAttributes).sort((a, b) => {
-    console.log(a, b);
-    if (dataAttributes[a].order > dataAttributes[b].order) {
-      return 1;
-    }
-    if (dataAttributes[b].order > dataAttributes[a].order) {
-      return -1;
-    }
-
-    return 0;
-  });
-
-  console.log(sortedAttributes);
-  const TimeSeriesCharts = sortedAttributes.map(attribute => (
+  const TimeSeriesCharts = ChartsToPlot.map(chart => (
     <TimeSeries
-      data={processedData[attribute]}
-      title={dataAttributes[attribute].title}
-      key={attribute}
+      data={processedData[chart.fieldName]}
+      title={chart.title}
+      key={chart.title}
     />
   ));
 
---
+++ src/utils/utils.js
@@ -0,0 +1,38 @@
+import { ChartsToPlot, StatesToInclude } from "../constants/Constants";
+
+// converts m/d/yy to yyyy-mm-dd
+export const convertDateToUNIXTime = date =>
+  new Date(
+    `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`
+  ).getTime();
+
+// used for data returned by https://covidtracking.com/api/
+export const processData = rawData => {
+  let processedDataTemp = {};
+  // current filter is for US states and excludes specific US cities
+  const dataRowFilter = dataRow => StatesToInclude.includes(dataRow.state);
+
+  const filteredData = rawData.filter(dataRow => dataRowFilter(dataRow));
+
+  filteredData.forEach(rawDataRow => {
+    const formattedDate = convertDateToUNIXTime(rawDataRow.date.toString(10));
+
+    ChartsToPlot.forEach(chart => {
+      if (!processedDataTemp[chart.fieldName]) {
+        processedDataTemp[chart.fieldName] = {};
+      }
+
+      if (!processedDataTemp[chart.fieldName][rawDataRow.state]) {
+        processedDataTemp[chart.fieldName][rawDataRow.state] = [];
+      }
+
+      processedDataTemp[chart.fieldName][rawDataRow.state].push([
+        formattedDate,
+        rawDataRow[chart.fieldName]
+      ]);
+    });
+  });
+
+  // TODO: need to sort data by date
+  return processedDataTemp;
+};
